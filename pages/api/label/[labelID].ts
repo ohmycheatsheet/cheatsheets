c1d729f05a335fe55b0bf57007b4c1b3975de3b8
jiangWeixian <jiangweixian1994@gmail.com>
2021-11-02 22:30:39 +0800

    :sparkles:  label by id

---
+++ pages/api/label/[labelID].ts
@@ -0,0 +1,16 @@
+import { NextApiResponse } from 'next'
+import { NextApiRequest } from '~/interface'
+import { withOmcs, withCors } from '~/utils/middlewares'
+
+export default withCors(
+  withOmcs(async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      const results = await req._omcs.getLabel(req.query.labelID)
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json')
+      res.end(JSON.stringify(results))
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: (err as any).message })
+    }
+  }),
+)
