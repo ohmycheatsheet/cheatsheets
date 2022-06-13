1daa51028e64e361020515747e2140170db7bb40
JiangWeixian <jiangweixian1994@gmail.com>
2022-06-13 23:18:15 +0800

    :recycle:  use react-ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import App from 'next/app'
-import React from 'react'
+import type { AppProps } from 'next/app'
+import React, { useEffect } from 'react'
 import Progress from 'nprogress'
+import { initialize, pageview } from 'react-ga'
 import Router from 'next/router'
 import { cheatSheetGlobalStyles } from '~/style/global'
 import { ThemeProvider } from 'mayumi/theme'
@@ -14,16 +15,20 @@ Router.events.on('routeChangeStart', () => Progress.start())
 Router.events.on('routeChangeComplete', () => Progress.done())
 Router.events.on('routeChangeError', () => Progress.done())
 
-class CustomApp extends App {
-  render() {
-    cheatSheetGlobalStyles()
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    )
-  }
+const CustomApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    if (process.env.G_ANALYTICS_ID && process.env.NODE_ENV === 'production') {
+      // Checks for GA ID and only turns on GA in production
+      initialize(process.env.G_ANALYTICS_ID)
+      pageview(window.location.pathname + window.location.search)
+    }
+  })
+  cheatSheetGlobalStyles()
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default CustomApp
